0e883fd31a94c8ea23e3bf7e17fb498e90ffd2d6
mwagnerretro <148266620+mwagnerretro@users.noreply.github.com>
2026-02-06 05:49:06 -0500

    Create Character class for battle arena

---
+++ rpg_arena_v2.py
@@ -0,0 +1,31 @@
+# The following program creates a battle arena for players.
+class Character:
+    # Constructor
+    def __init__(self, name, strength, health, defense):
+        self.name = name
+        self.strength = strength
+        self.health = health
+        self.defense = defense
+
+    # take_damage() Method
+    def take_damage(self, damage):
+        damage_taken = damage - self.defense
+        self.health -= damage_taken
+        return damage_taken
+
+    # attack() Method
+    def attack(self, target):
+        damage = self.strength * 2
+        damage_dealt = target.take_damage(damage)
+        return damage_dealt
+
+    # is_alive() Method
+    def is_alive(self):
+        return self.health > 0
+
+# Class Objects
+player = Character("Cheetah", 10, 100, 2)
+print(player.name)
+print(player.strength)
+print(player.health)
+print(player.defense)
